ddcdcc17e43141450ed140c52b5a06f1ab4cd085
Jim Kratus <32602483+jkrat@users.noreply.github.com>
2019-11-22 12:01:03 -0600

    install and configure devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :employees
   root 'home#index', as: 'home_index'
   resources :transactions do
     get :void, on: :member
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_164912) do
+ActiveRecord::Schema.define(version: 2019_11_22_175143) do
 
   create_table "employees", force: :cascade do |t|
     t.string "first_name", null: false
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(version: 2019_10_01_164912) do
     t.integer "status", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.index ["email"], name: "index_employees_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
   end
 
   create_table "transactions", force: :cascade do |t|
